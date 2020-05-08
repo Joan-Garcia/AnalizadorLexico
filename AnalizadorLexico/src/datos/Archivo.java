a9e837d1de0b7ca2e8a0f73d5f1c1f8d8099afd7
Joan Garcia <joangarcia2015@gmail.com>
2020-05-08 16:59:21 -0500

    Abrir y leer archivo.

---
+++ AnalizadorLexico/src/datos/Archivo.java
@@ -0,0 +1,53 @@
+
+package datos;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class Archivo {
+    File archivo;
+    JFileChooser chooser;
+    FileReader fr;
+    BufferedReader br;
+    
+    public Archivo() {
+        chooser = new JFileChooser();
+    }
+    
+    public String seleccionaArchivo() {
+        chooser.setFileFilter(new FileNameExtensionFilter(".txt", "txt"));
+        chooser.setDialogTitle("Selecciona un archivo.");
+        
+        if (chooser.showDialog(null, "Ok") == JFileChooser.CANCEL_OPTION)
+            return null;
+        
+        archivo = chooser.getSelectedFile();
+        return archivo.getAbsolutePath();
+    }
+    
+    public String leerArchivo() {
+        String cadena = "", linea;
+        try {
+            fr = new FileReader (seleccionaArchivo());
+            br = new BufferedReader(fr);
+
+            while ((linea=br.readLine())!=null) {
+                cadena = cadena + linea;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if( null != fr )
+                    fr.close();               
+            }catch (Exception e2){ 
+                e2.printStackTrace();
+            }
+        }
+        return cadena;
+    }
+}
---
+++ AnalizadorLexico/src/pruebas/PruebaArchivo.java
@@ -0,0 +1,11 @@
+
+package pruebas;
+
+import datos.Archivo;
+
+public class PruebaArchivo {
+    public static void main(String[] args) {
+        Archivo a = new Archivo();
+        System.out.println(a.leerArchivo());
+    }
+}
