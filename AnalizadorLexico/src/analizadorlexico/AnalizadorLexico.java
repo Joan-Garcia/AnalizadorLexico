62b2705ef953f5db7558bbe4dcd60d0bc3a84525
MiguelLPmez <mig.angel.1916@gmail.com>
2020-05-10 19:45:18 -0500

    Autómata sin completar +

--- AnalizadorLexico/src/analizadorlexico/AnalizadorLexico.java
+++ AnalizadorLexico/src/analizadorlexico/AnalizadorLexico.java
@@ -38,121 +38,199 @@ public class AnalizadorLexico {
       tokens.add(new ArrayList());
   }
   
-  @SuppressWarnings("UnusedAssignment")
-  public void procesa(String programa){
-    boolean error;
-    String palabra;
-    int estado, inicio, fin;
-    
-    error = false;
-    palabra = "";
-    estado = inicio = fin = 0;
-      System.out.println(programa.length());
-    while(fin <= (programa.length()-1))
-      switch(estado){
-        case 0:
-          if(esMinuscula(programa.charAt(inicio))){                               //Si la palabra empieza con minúscula, es una palabra reservada o error.
-            palabra += programa.charAt(inicio);
-            estado = 1;
-            break;
-          } else if (!esMinuscula(programa.charAt(inicio))){                      //Si la palabra empieza con mayúscula.
-            palabra += programa.charAt(inicio);
-            estado = 3;
-            break;
-          }
-        case 1:                                                                   //Verifica que la palabra es una palabra reservada o un error.
-          while(!esEspacio(programa.charAt(fin + 1)) && (fin + 1)<=(programa.length())){                            //Mientras el siguiente caracter no sea un espacio
-            if(esMinuscula(programa.charAt(fin + 1))){                            //si es minúscula, puede ser una palabra reservada.
-              palabra += programa.charAt(fin + 1);
-              fin++;
-                System.out.println(palabra);
-            }else{                                                                //si no, es un error.
-              error = true;
-              palabra += programa.charAt(fin + 1);
-              fin++;
-            }
-          }
-          if(!error){                                                             //Terminamos de analizar la palabra, si no es error...
-            if(esPalabraReservada(palabra)){                                      //Si esta en lista de palabras reservadas
-              palabrasReservadas.add(palabra);                                    //Agregala a la tabla de palabras reservadas.
-            } else {                                                              //Si no está, es un error:
-              errores.add(palabra);                                               //Añadir a tabla de errores
-            }
-          } else {
-              errores.add(palabra);
-          }
-          //Se termina el análisis:
-          palabra = "";
-          fin+=2;
-          inicio = fin;
-          estado = 0;
-          error = false;
-          break;
-        case 3:
-          if((fin + 1)<=(programa.length()))
-          while(!esEspacio(programa.charAt(fin + 1)) && (fin + 1)<=(programa.length())){                            //Mientras no siga un espacio
-            if(esMinuscula(programa.charAt(fin + 1)) ||                           //Verifica si es minúscula, guión bajo o número
-               esGuionBajo(programa.charAt(fin + 1)) ||
-               esNumero(programa.charAt(fin + 1))){
-              palabra += programa.charAt(fin + 1);
-              fin++;
-            }else{                                                                //si no, es un error.
-              error = true;
-              palabra += programa.charAt(fin + 1);
-              fin++;
-            }
-          }
-          if(esGuionBajo(palabra.charAt(palabra.length()-1)))                       //Verifica que el último caracter de la palabra no sea, 
-            error = true;                                                         //guión bajo.
-          if(!error){                                                             //Terminamos de analizar la palabra, si no es error...
-            simbolos.get(0).add(palabra);                                         //Agrega la palabra a la columna lexema en la tabla se simbolos.
-            simbolos.get(1).add("Identificador");                                 //Agrega su clasificación.
-            tokens.get(0).add(palabra);                                           //Agrega la palabra a la columna lexema a la tabla de tokens.
-            tokens.get(0).add("Identificador");                                   //Agrega su clasificación.
-            tokens.get(0).add("400");                                             //Agrega el Atributo de Identificador.
-          } else {
-            errores.add(palabra);
-          }
-          //Se termina el análisis:
-          palabra = "";
-          inicio = fin;
-          estado = 0;
-          error = false;
-          break;
-
-      }
-    
-  }
+//  @SuppressWarnings("UnusedAssignment")
+//  public void procesa(String programa){
+//    boolean error;
+//    String palabra;
+//    int estado, inicio, fin;
+//    
+//    error = false;
+//    palabra = "";
+//    estado = inicio = fin = 0;
+//      System.out.println(programa.length());
+//    while(fin <= (programa.length()-1))
+//      switch(estado){
+//        case 0:
+//          if(esMinuscula(programa.charAt(inicio))){                               //Si la palabra empieza con minúscula, es una palabra reservada o error.
+//            palabra += programa.charAt(inicio);
+//            estado = 1;
+//            break;
+//          } else if (!esMinuscula(programa.charAt(inicio))){                      //Si la palabra empieza con mayúscula.
+//            palabra += programa.charAt(inicio);
+//            estado = 3;
+//            break;
+//          }
+//        case 1:                                                                   //Verifica que la palabra es una palabra reservada o un error.
+//          while(!esEspacio(programa.charAt(fin + 1)) && (fin + 1)<=(programa.length())){                            //Mientras el siguiente caracter no sea un espacio
+//            if(esMinuscula(programa.charAt(fin + 1))){                            //si es minúscula, puede ser una palabra reservada.
+//              palabra += programa.charAt(fin + 1);
+//              fin++;
+//                System.out.println(palabra);
+//            }else{                                                                //si no, es un error.
+//              error = true;
+//              palabra += programa.charAt(fin + 1);
+//              fin++;
+//            }
+//          }
+//          if(!error){                                                             //Terminamos de analizar la palabra, si no es error...
+//            if(esPalabraReservada(palabra)){                                      //Si esta en lista de palabras reservadas
+//              palabrasReservadas.add(palabra);                                    //Agregala a la tabla de palabras reservadas.
+//            } else {                                                              //Si no está, es un error:
+//              errores.add(palabra);                                               //Añadir a tabla de errores
+//            }
+//          } else {
+//              errores.add(palabra);
+//          }
+//          //Se termina el análisis:
+//          palabra = "";
+//          fin+=2;
+//          inicio = fin;
+//          estado = 0;
+//          error = false;
+//          break;
+//        case 3:
+//          if((fin + 1)<=(programa.length()))
+//          while(!esEspacio(programa.charAt(fin + 1)) && (fin + 1)<=(programa.length())){                            //Mientras no siga un espacio
+//            if(esMinuscula(programa.charAt(fin + 1)) ||                           //Verifica si es minúscula, guión bajo o número
+//               esGuionBajo(programa.charAt(fin + 1)) ||
+//               esNumero(programa.charAt(fin + 1))){
+//              palabra += programa.charAt(fin + 1);
+//              fin++;
+//            }else{                                                                //si no, es un error.
+//              error = true;
+//              palabra += programa.charAt(fin + 1);
+//              fin++;
+//            }
+//          }
+//          if(esGuionBajo(palabra.charAt(palabra.length()-1)))                       //Verifica que el último caracter de la palabra no sea, 
+//            error = true;                                                         //guión bajo.
+//          if(!error){                                                             //Terminamos de analizar la palabra, si no es error...
+//            simbolos.get(0).add(palabra);                                         //Agrega la palabra a la columna lexema en la tabla se simbolos.
+//            simbolos.get(1).add("Identificador");                                 //Agrega su clasificación.
+//            tokens.get(0).add(palabra);                                           //Agrega la palabra a la columna lexema a la tabla de tokens.
+//            tokens.get(0).add("Identificador");                                   //Agrega su clasificación.
+//            tokens.get(0).add("400");                                             //Agrega el Atributo de Identificador.
+//          } else {
+//            errores.add(palabra);
+//          }
+//          //Se termina el análisis:
+//          palabra = "";
+//          inicio = fin;
+//          estado = 0;
+//          error = false;
+//          break;
+//
+//      }
+//    
+//  }
   
   public void automata(String programa){
     String palabra;
-    int estado, inicio, fin;
+    int estado, inicio;
     
     palabra = "";
-    estado = inicio = fin = 0;
+    estado = inicio = 0;
     
-    switch(estado){
+    while(inicio != programa.length())
+    switch(estado){ //(...) = 5. + .05
       case 0:
-        if(esSimbolo(programa.charAt(inicio))){ 
+        if (esSimbolo(programa.charAt(inicio))){ 
           estado = 2;                                   
           break;
         } else if(esNumero(programa.charAt(inicio))){
           estado = 5;
           break;
         } else if(esMayuscula(programa.charAt(inicio))){
-          
+          palabra += programa.charAt(inicio);
+          inicio++;
+          estado = 3;
+          break;
+        } else if(esMinuscula(programa.charAt(inicio))){
+          estado = 1;
+          break;
+        } else if(esEspacio(programa.charAt(inicio))){
+          inicio++;
+          estado = 0;
+          break;
+        } else {                                      //Es caracter no permitido
+          palabra += programa.charAt(inicio);
+          if(esPunto(programa.charAt(inicio))){
+            while(esNumero(programa.charAt(inicio + 1))){
+              palabra += programa.charAt(inicio + 1);
+              inicio++;
+            }
+          }
+          if(esEspacio(programa.charAt(inicio + 1))/*|| 
+             esSimbolo(programa.charAt(inicio + 1))  ||
+             esNumero(programa.charAt(inicio + 1))   ||
+             esMinuscula(programa.charAt(inicio + 1))||
+             esMayuscula(programa.charAt(inicio + 1))  
+             */){
+            /* 
+              Añadir a errores
+            */
+            palabra = "";
+          }
+          inicio++;
+          estado = 0;
+          break;
         }
-        
+      case 1:                                               //PALABRASRESERVADAS
+        if(esMinuscula(programa.charAt(inicio))){
+          palabra += programa.charAt(inicio);
+          inicio++;
+          estado = 1;
+          break;
+        } else {
+          if(esPalabraReservada(palabra)){
+            /* Añadirla a las listas **
+              Símbolo. Token = tokenPalabraRervada(palabra)
+            */
+            palabra = "";
+            inicio++;
+            estado = 0;
+          } else {
+            /* 
+              Añadirla a lista de erorres **
+            */
+            palabra = "";
+            inicio++;
+            estado = 0;
+          }
+        }
       case 2:                                               // SÍMBOLO
         if(esSimbolo(programa.charAt(inicio))){
           /* Añadir a las listas ***
             Símbolo. Token = ASCII
           */
           inicio++;
-          fin++;
           estado = 2;
           break;
         } else {
+          estado = 0;
+          break;
+        }
+      case 3:                                               //IDENTIFICADOR         
+        if(esMinuscula(programa.charAt(inicio))){
+          palabra += programa.charAt(inicio);
+          inicio++;
+          estado = 3;
+          break;
+        } else if(esGuionBajo(programa.charAt(inicio))){
+          palabra += programa.charAt(inicio);
+          inicio++;
+          estado = 3;
+          break;
+        } else if(esNumero(programa.charAt(inicio))){
+          palabra += programa.charAt(inicio);
+          inicio++;
+          estado = 3;
+          break;
+        } else{
+          /* Añadir a las listas ***
+            Identificador. Token = 400
+          */
+          palabra = "";
           estado = 0;
           break;
         }
@@ -160,16 +238,23 @@ public class AnalizadorLexico {
         if(esNumero(programa.charAt(inicio))){              // NÚMERO ENTERO
           palabra += programa.charAt(inicio);             //Guardamos el número
           inicio++;
-          fin++;
           estado = 5;
           break;
-        } else if (esPunto(programa.charAt(inicio))){       // sigue un punto.
-          palabra += programa.charAt(inicio);             //Guardamos el punto
-          inicio++;
-          fin++;
-          estado = 6;                                     //Es flotante
-          break;
-        } else{                                       //Termina el número entero
+        } else if (esPunto(programa.charAt(inicio))){  // sigue un punto.
+          palabra += programa.charAt(inicio);          //Guardamos el punto
+          if (!esNumero(programa.charAt(inicio + 1))){
+            /* 
+              Añadirla a lista de erorres **
+            */
+            palabra = "";
+            inicio++;
+            estado = 0;
+          } else {           
+            inicio++;
+            estado = 6;                                     //Es flotante
+            break;
+          }
+        } else {                                       //Termina el número entero
           /* Añadir a las listas ***
             Número entero. Token = 300
           */
@@ -181,7 +266,6 @@ public class AnalizadorLexico {
         if(esNumero(programa.charAt(inicio))){
           palabra += programa.charAt(inicio); 
           inicio++;
-          fin++;
           estado = 6;
           break;
         } else {                                      //Termina el número float
@@ -192,7 +276,7 @@ public class AnalizadorLexico {
           estado = 0;
           break;
         }
-        
+      
     }
   }
   
@@ -209,6 +293,10 @@ public class AnalizadorLexico {
     return Character.isLowerCase(c);
   }
   
+  private boolean esMayuscula(char c){
+    return Character.isUpperCase(c);
+  }
+  
   private boolean esEspacio(char c){
     return Character.isSpaceChar(c);
   }
@@ -221,6 +309,19 @@ public class AnalizadorLexico {
     return Character.isDigit(c);
   }
   
+  private int valorASCII(char c){
+    return (int) c;
+  }
+  
+  // Devuelve el token correspondiente a la palabra reservada.
+  private int tokenPalabraReservada(String palabra){
+    for (int i = 0; i < listaPalabrasReservadas[0].length; i++)
+      if(palabra.equals(listaPalabrasReservadas[i][0]))
+        return Integer.parseInt(listaPalabrasReservadas[i][1]);
+    return -1;
+  }
+  
+  // Comprueba si una palabra forma parte de las palabras reservadas.
   private boolean esPalabraReservada(String palabra){
     for (int i = 0; i < listaPalabrasReservadas[0].length; i++)
       if(palabra.equals(listaPalabrasReservadas[i][0]))
